1121e3e2d565297f05b5676f47b1537c5794dce7
Dawid Nogacz <sloenthran@gmail.com>
2019-08-08 17:54:19 +0200

    Add swagger

--- src/main/java/pl/nogacz/tasks/config/CoreConfiguration.java
+++ src/main/java/pl/nogacz/tasks/config/CoreConfiguration.java
@@ -3,14 +3,29 @@ package pl.nogacz.tasks.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Dawid Nogacz on 02.08.2019
  */
+@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.nogacz.tasks.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
