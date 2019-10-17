d5088eb16423ef88b0a77b15e57b0828c0fdc251
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2019-10-17 16:23:08 +0200

    Arithmetic operations in Thymeleaf

--- src/main/java/pl/nogacz/tasks/controller/StaticWebPageController.java
+++ src/main/java/pl/nogacz/tasks/controller/StaticWebPageController.java
@@ -3,10 +3,15 @@ package pl.nogacz.tasks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 public class StaticWebPageController {
     @GetMapping(value = "/")
-    public String index() {
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
         return "index";
     }
 }
