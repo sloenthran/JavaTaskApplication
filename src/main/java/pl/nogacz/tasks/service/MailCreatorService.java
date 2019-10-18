99cc1187e6807e0f43698e46d12f589f09475a84
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2019-10-18 14:24:57 +0200

    Update mail template

--- src/main/java/pl/nogacz/tasks/service/MailCreatorService.java
+++ src/main/java/pl/nogacz/tasks/service/MailCreatorService.java
@@ -2,9 +2,11 @@ package pl.nogacz.tasks.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import pl.nogacz.tasks.config.AdminConfig;
 
 @Service
 @AllArgsConstructor
@@ -12,9 +14,23 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.mail}")
+    private String companyMail;
+
+    private AdminConfig adminConfig;
+
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://sloenthran.github.io/JavaTaskApplication/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "See you later...");
+        context.setVariable("company_name", companyName);
+        context.setVariable("company_mail", companyMail);
         return templateEngine.process("mail/created-trello-card", context);
     }
 }
