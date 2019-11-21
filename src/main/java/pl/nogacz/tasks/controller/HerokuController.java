5bf983e81e6e33d7c44f01abdb1090862fefcf8a
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2019-11-21 18:32:29 +0100

    Change mapping in heroku controller

--- src/main/java/pl/nogacz/tasks/controller/HerokuController.java
+++ src/main/java/pl/nogacz/tasks/controller/HerokuController.java
@@ -3,6 +3,7 @@ package pl.nogacz.tasks.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,12 @@ import java.io.IOException;
 public class HerokuController {
     private Logger LOGGER = LoggerFactory.getLogger(HerokuController.class);
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public void homepage(HttpServletResponse response) throws IOException {
         response.sendRedirect("https://sloenthran.github.io/JavaTaskApplication/");
     }
 
-    @RequestMapping("/heroku/wakeUp")
+    @GetMapping("/heroku/wakeUp")
     public void wakeUp() {
         LOGGER.info("App wakeUp");
     }
